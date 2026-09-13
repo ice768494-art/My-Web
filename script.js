0a0249896690690d66b3b81a87dcb45ac15ac5ca
ice768494-art <ice768494@gmail.com>
2026-09-13 19:02:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("telegramButton");
 
 button.addEventListener("click", function () {
-    window.open("https://t.me/@AnimeNetworkTamil", "_blank");
+    window.open("https://t.me/AnimeNetworkTamil", "_blank");
 });
